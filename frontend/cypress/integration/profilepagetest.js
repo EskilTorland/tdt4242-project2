7168edb5ad002096047209269a33be3415357aa2
Trym <trymj@stud.ntnu.no>
2022-03-17 13:40:32 +0100

    added cypress tests for challenge page

--- frontend/cypress/integration/profilepagetest.js
+++ frontend/cypress/integration/profilepagetest.js
@@ -1,26 +1,22 @@
 var baseUrl = "localhost:9090";
 
 describe('Boundary tests for view/edit exercise page', () => {
+    before(() => {
+        // cy.RegisterDummyUser();
+      })
+
     beforeEach(() => {
-        //cy.RegisterDummyUser();
-        cy.LoginDummyUser();
         cy.visit(baseUrl+"/index.html");
         })
 
   it('Check that profile button is not visible when user is not logged in', () => {
-    cy.get('#btn-logout').click();
-    cy.wait(500);
-    cy.get('#btn-profile').should('not.exist');
+    cy.get('#btn-profile').should('not.be.visible');
   })
 
-  it('Login user and check that information is displayed', () => {
+  it('Login user can go to their profile', () => {
+    cy.LoginDummyUser();
+    cy.wait(500);
     cy.get('#btn-profile').should('be.visible');
     cy.get('#btn-profile').click();
-    cy.url().contains('/profile.html');
-
-    cy.get('#form-phone').contains('1234')
-    cy.get('#form-country').contains('Norge')
-    cy.get('#form-city').contains('Oslo')
-    cy.get('#form-address').contains('Bjørnlia 4')
   })
 })
