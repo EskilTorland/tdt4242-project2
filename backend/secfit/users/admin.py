c5413cd561db673d1f47b9d0d5ffe6e531ad9252
Eskil <eskil.torland@gmail.com>
2022-03-31 13:35:31 +0200

    made changes to imports

--- backend/secfit/users/admin.py
+++ backend/secfit/users/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Offer, AthleteFile
 from django.contrib.auth import get_user_model
 from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import Offer, AthleteFile
 
 # Register your models here.
 
