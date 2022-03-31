a7f860d3afea4a8e1df6258c6b73c7324c9a48f9
Eskil <eskil.torland@gmail.com>
2022-03-31 12:45:17 +0200

    Made readability of users/urls.py better

--- backend/secfit/users/urls.py
+++ backend/secfit/users/urls.py
@@ -2,13 +2,30 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path("api/users/", views.UserList.as_view(), name="user-list"),
-    path("api/users/<int:pk>/", views.UserDetail.as_view(), name="user-detail"),
-    path("api/users/<str:username>/", views.UserDetail.as_view(), name="user-detail"),
-    path("api/offers/", views.OfferList.as_view(), name="offer-list"),
-    path("api/offers/<int:pk>/", views.OfferDetail.as_view(), name="offer-detail"),
     path(
-        "api/athlete-files/", views.AthleteFileList.as_view(), name="athlete-file-list"
+        "api/users/", 
+        views.UserList.as_view(), 
+        name="user-list"),
+    path(
+        "api/users/<int:pk>/", 
+        views.UserDetail.as_view(), 
+        name="user-detail"),
+    path(
+        "api/users/<str:username>/", 
+        views.UserDetail.as_view(), 
+        name="user-detail"),
+    path(
+        "api/offers/", 
+        views.OfferList.as_view(), 
+        name="offer-list"),
+    path(
+        "api/offers/<int:pk>/", 
+        views.OfferDetail.as_view(), 
+        name="offer-detail"),
+    path(
+        "api/athlete-files/", 
+        views.AthleteFileList.as_view(),
+        name="athlete-file-list"
     ),
     path(
         "api/athlete-files/<int:pk>/",
